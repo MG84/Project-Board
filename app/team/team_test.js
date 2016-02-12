316b2dbc0ebb6dbcd61f2900131d0ae6942668d2
Marco Giannone <marcogiannone@Marcos-MacBook-Pro.local>
2016-02-12 15:46:02 +0100

    Fixed tests for team.js

--- app/team/team_test.js
+++ app/team/team_test.js
@@ -1,16 +1,25 @@
 'use strict';
 
 describe('projectBoard.team module', function() {
+  var scope, ctrl, $httpBackend;
 
   beforeEach(module('projectBoard.team'));
 
-  describe('team controller', function(){
+  describe('TeamCtrl', function(){
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var teamCtrl = $controller('TeamCtrl');
-      expect(teamCtrl).toBeDefined();
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+        $httpBackend = _$httpBackend_;
+        $httpBackend.expectGET('team/team.json').respond([{name: 'member 1'}, {name: 'member 2'}]);
+
+        scope = $rootScope.$new();
+        ctrl = $controller('TeamCtrl', {$scope:scope});
     }));
+    it('should create team model with two team member fetched from xhr', function() {
+      expect(scope.team).toBeUndefined();
+      $httpBackend.flush();
 
+      expect(scope.team).toEqual([{name: 'member 1'}, {name: 'member 2'}]);
+    });
   });
 });
